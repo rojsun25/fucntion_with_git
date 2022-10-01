498860ceb9871676d5c45f993bbbe88ba5972188
Rojsun <rojsun25@gmail.com>
2022-10-01 22:59:48 +0100

    first commit added .gitignore for ipynb files to the repository

---
+++ generators.py
@@ -0,0 +1,24 @@
+# def square_numbers(nums) :
+#     result = []
+#     for i in nums:
+#         result.append(i*i)
+#     return result
+
+# my_nums = square_numbers ([1,2,3,4,5])
+# #my_nums = [x*x for x in [1,2,3,4,5]]
+
+# print (my_nums) # [1 , 4, 9 , 16 , 25]
+
+# # for num in my_nums:
+# # print num
+
+
+
+# import numpy as np
+
+# a = np.array([1,2,3,4])
+# print(a[])
+
+x ={1,2}
+x.add(5)
+print(x)
